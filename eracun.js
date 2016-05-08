cb61a90b738d3a040fbbb886b5ac83e0af1a8188
Rok Grmek <grmekrok@gmail.com>
2016-05-08 12:55:53 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,7 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
@@ -142,14 +142,31 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
-})
+  var form = new formidable.IncomingForm();
+
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+    var racunId = polja.seznamRacunov;
+    strankaIzRacuna(racunId, function(stranke) {
+      pesmiIzRacuna(racunId, function(pesmi) {
+        for (var i=0; i<pesmi.length; i++) {
+          pesmi[i].stopnja = davcnaStopnja(pesmi[i].izvajalec, pesmi[i].zanr);
+        }
+        odgovor.setHeader('content-type', 'text/xml');
+        odgovor.render('eslog', {
+          vizualiziraj: true,
+          postavkeRacuna: pesmi,
+          stranka: stranke[0]
+        });
+      });
+    });
+  });
+});
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
 streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
